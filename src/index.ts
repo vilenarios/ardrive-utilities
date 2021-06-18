685f5ba09945a1e350b38ee5bad5991721bc4bf4
Philip Mataras <vilenarios@gmail.com>
2021-06-18 17:32:24 -0400

    Checks for broken public root folders

--- src/index.ts
+++ src/index.ts
@@ -4,38 +4,43 @@ import * as wallet from "./wallet";
 import * as gql from "./gql";
 import * as common from "./common";
 import { ArFSDriveEntity } from "./types/arfs_Types";
+import { checkForBrokenRootFolder } from "./utilities";
 
 async function main() {
 
 	// Get the wallet that owns the broken drives
-	const localWallet = await cli.promptForLocalWalletPath();
+	//const localWallet = await cli.promptForLocalWalletPath();
+	const localWallet = "C:\\stuff\\my_shared_demo_key2.json"
 	const cachedWallet = await wallet.getCachedWallet(localWallet);
 	const owner = cachedWallet.walletPublicKey;
 	const walletBalance = await wallet.getWalletBalance(owner);
-	const walletPrivateKey = JSON.stringify(cachedWallet.walletPrivateKey);
+	// const walletPrivateKey = JSON.stringify(cachedWallet.walletPrivateKey);
 
-	// Get the drive password for any private drives
-	// This should only be called if private drives are found
-	const drivePassword = await cli.promptForDrivePassword();
-
-	console.log (walletBalance)
-	console.log (walletPrivateKey)
-	console.log (drivePassword)
+	console.log ("Loaded Wallet: %s", owner)
+	console.log ("Your balance is %s", walletBalance)
 
 	// Get all of the public drives for the user, starting at block 0
+	console.log ("Getting all your public drives and checking for broken root folders")
 	let publicDrives : ArFSDriveEntity[] | string = await gql.getAllLatestPublicDriveEntities(owner, 0);
+
 	if (typeof publicDrives === 'string') {
 		console.log (publicDrives); // Error
 	} else {
 		await common.asyncForEach(publicDrives, async (publicDrive: ArFSDriveEntity) => {
-			// Get the drive's root folder transaction
-
+			// Check the root folder for each drive
+			console.log ("Drive Name: %s, Root Folder ID %s, Drive ID: %s", publicDrive.name, publicDrive.rootFolderId, publicDrive.driveId);
+			const broken = await checkForBrokenRootFolder(owner, publicDrive.rootFolderId)
+			if (broken) {
+				const answer = await cli.promptForFix(publicDrive.name, "root folder", publicDrive.rootFolderId)
+				if (answer) {
+					console.log ("Fixing!")
+				}
+			}
 		})
-
-
 	}
 
-	// Do any of them have b
+	// Get the drive password for any private drives
+	// const drivePassword = await cli.promptForDrivePassword();
 }
 
 function displayBanner() {
